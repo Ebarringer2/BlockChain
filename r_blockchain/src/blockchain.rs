7495b1c286a9a14f99a0ca195b811736632c0c30
Elliott Barringer <elliottbarringer@icloud.org>
2024-01-02 22:01:48 -0800

    first push at merkle tree implementation

--- r_blockchain/src/blockchain.rs
+++ r_blockchain/src/blockchain.rs
@@ -1,6 +1,8 @@
 use ring::digest;
 use rand::Rng;
 
+type HASH = Vec<u8>;
+
 pub fn generate_rand_string() -> String {
     const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const STRING_LENGTH: usize = 10;
@@ -14,7 +16,7 @@ pub fn generate_rand_string() -> String {
     random_string
 }
 
-pub fn calculate_hash(data: &str) -> Vec<u8> {
+pub fn calculate_hash(data: String) -> HASH {
     let mut context = digest::Context::new(&digest::SHA256);
     context.update(data.as_bytes());
     context.finish().as_ref().to_vec()
@@ -34,6 +36,12 @@ pub struct Pow {
     pub difficulty: i32
 }
 
+pub struct MerkleTree {
+    block: Block,
+    transactions: Vec<String>,
+    tree: Vec<HASH>
+}
+
 impl Block {
     pub fn new(index: i32, owner: String, timestamp: i32, transactions: Vec<String>, proof: i32, prev_hash: String) -> Self {
         Block {
@@ -51,7 +59,7 @@ impl Pow {
     pub fn new(block: Block, difficulty: i32) -> Self {
         Pow {block, difficulty}
     }
-    pub fn hash(&self, input: String) -> Vec<u8> {
+    pub fn hash(&self, input: String) -> HASH {
         let mut context = digest::Context::new(&digest::SHA256);
         context.update(input.as_bytes());
         context.finish().as_ref().to_vec()
@@ -69,10 +77,37 @@ impl Pow {
                 self.block.transactions.join(","),
                 nonce
             );
-            let hash: Vec<u8> = self.hash(input_data);
+            let hash: HASH = self.hash(input_data);
             if hash.starts_with(&target_prefix) {
                 return nonce;
             }
         }
+    }
+}
+
+impl MerkleTree {
+    pub fn new(block: Block) -> Self {
+        MerkleTree {
+            block: block,
+            transactions: block.transactions,
+            tree: vec![]
+        }
+    }
+    pub fn build_tree(&self) {
+        if self.transactions.is_empty() {
+            self.tree = vec![];
+        }
+        let mut tree: Vec<HASH> = self.transactions.iter().map(|tx: &String| self.hash_transaction(tx)).collect();
+        while tree.len() > 1 {
+            tree = self.combine_pairs(tree);
+        self.tree = tree;
+        }
+    }
+    pub fn hash_transaction(&self, transaction: String) -> HASH {
+        calculate_hash(transaction)
+    }
+    pub fn combine_hashes(&self, left: HASH, right: HASH) -> HASH {
+        left.extend(right);
+        calculate_hash();
     }
 }
